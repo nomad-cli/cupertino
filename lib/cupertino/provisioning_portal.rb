73d0297f4a593b9ecc68ba868083b9cd33322b9a
Thomas Dohmke <thomas@dohmke.de>
2012-07-17 00:42:41 +0200

    Moving types into separate file to reduce dependency with provisioning_portal.rb.

--- lib/cupertino/provisioning_portal.rb
+++ lib/cupertino/provisioning_portal.rb
@@ -1,35 +1,6 @@
 require 'mechanize'
 
-module Cupertino
-  module ProvisioningPortal
-    class Device < Struct.new(:name, :udid)
-      def to_s
-        "#{self.name} #{self.udid}"
-      end
-    end
-
-    class Certificate < Struct.new(:name, :type, :provisioning_profiles, :expiration_date, :status)
-      def to_s
-        "#{self.name}"
-      end
-    end
-
-    class AppID < Struct.new(:bundle_seed_id, :description, :development_properties, :distribution_properties)
-      def to_s
-        "#{self.bundle_seed_id}"
-      end
-    end
-
-    class ProvisioningProfile < Struct.new(:name, :type, :app_id, :status)
-      def to_s
-        "#{self.name}"
-      end
-    end
-
-    class UnsuccessfulAuthenticationError < RuntimeError; end
-  end
-end
-
+require 'cupertino/provisioning_portal/types'
 require 'cupertino/provisioning_portal/helpers'
 require 'cupertino/provisioning_portal/agent'
 require 'cupertino/provisioning_portal/commands'
---
+++ lib/cupertino/provisioning_portal/types.rb
@@ -0,0 +1,29 @@
+module Cupertino
+  module ProvisioningPortal
+    class Device < Struct.new(:name, :udid)
+      def to_s
+        "#{self.name} #{self.udid}"
+      end
+    end
+
+    class Certificate < Struct.new(:name, :type, :provisioning_profiles, :expiration_date, :status)
+      def to_s
+        "#{self.name}"
+      end
+    end
+
+    class AppID < Struct.new(:bundle_seed_id, :description, :development_properties, :distribution_properties)
+      def to_s
+        "#{self.bundle_seed_id}"
+      end
+    end
+
+    class ProvisioningProfile < Struct.new(:name, :type, :app_id, :status)
+      def to_s
+        "#{self.name}"
+      end
+    end
+
+    class UnsuccessfulAuthenticationError < RuntimeError; end
+  end
+end

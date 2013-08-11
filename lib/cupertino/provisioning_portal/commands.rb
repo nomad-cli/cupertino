b1411ef4daca78c7297737082a2048ab275ecf29
Matt "Trip" Maker <trip@monstro.us>
2013-08-11 18:24:22 -0500

    add global options --info and --debug

--- lib/cupertino/provisioning_portal/commands.rb
+++ lib/cupertino/provisioning_portal/commands.rb
@@ -1,6 +1,9 @@
 include Cupertino::ProvisioningPortal
 include Cupertino::ProvisioningPortal::Helpers
 
+global_option('-i','--info', 'Set log level to INFO and higher') { agent.log_level(Logger::INFO) }
+global_option('-d','--debug','Set log level to DEBUG and higher (that is, all log messages)') { agent.log_level(Logger::DEBUG) }
+
 require 'cupertino/provisioning_portal/commands/certificates'
 require 'cupertino/provisioning_portal/commands/devices'
 require 'cupertino/provisioning_portal/commands/profiles'
